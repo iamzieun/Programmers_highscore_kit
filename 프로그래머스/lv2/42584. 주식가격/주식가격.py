a1dbd136e8a8217e426535a17aecc95eb75605fc
박지은 | Hailey Park <97666193+iamzieun@users.noreply.github.com>
2023-03-30 15:04:10 +0900

    [level 2] Title: 주식가격, Time: 36.72 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -3,15 +3,14 @@ from collections import deque
 def solution(prices):
     prices = deque(prices)
     answer = []
+    
     while prices:
         price = prices.popleft()
         not_fall = 0
         for n_price in prices:
-            if n_price >= price:
-                not_fall += 1
-            else:
-                not_fall += 1
-                break
+            not_fall += 1
+            if n_price >= price: continue
+            else: break
         answer.append(not_fall)
 
     return answer
