9e1afc090dddb8f700d9e499ab991cb77b94d021
박지은 | Hailey Park <97666193+iamzieun@users.noreply.github.com>
2023-04-04 15:59:18 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 33.51 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,8 +1,7 @@
 from collections import Counter
 
 def solution(participant, completion):
-    part_counter = Counter(participant)
-    comp_counter = Counter(completion)
+    part_counter, comp_counter = Counter(participant), Counter(completion)
     
     for runner in comp_counter.keys():
         if part_counter[runner] > comp_counter[runner]:
