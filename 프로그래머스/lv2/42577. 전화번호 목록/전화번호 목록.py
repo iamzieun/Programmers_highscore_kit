cc95ae1afb90595055ec20bcec93898a72e7c72a
박지은 | Hailey Park <97666193+iamzieun@users.noreply.github.com>
2023-04-06 10:20:54 +0900

    [level 2] Title: 전화번호 목록, Time: 195.18 ms, Memory: 34.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,15 @@
 def solution(phone_book):
-    phone_book.sort()
-    #print(phone_book)
-    for i in range(len(phone_book)-1):
-        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
-            return False
-            break
+    # 1. Hash map 생성
+    hash_map = {}
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+    
+    # 2. 접두어가 Hash map에 존재하는지 찾기
+    for phone_number in phone_book:
+        prefix = ""
+        for number in phone_number:
+            prefix += number
+            # 3. 자기 자신 제외
+            if prefix in hash_map and prefix != phone_number:
+                return False
     return True
